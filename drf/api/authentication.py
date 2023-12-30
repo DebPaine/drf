a9630807541888a8b228df698653af01a66e2ed5
unknown <debmalyapaine@gmail.com>
2023-12-30 21:16:11 +0530

    add: token auth notes

--- drf/api/authentication.py
+++ drf/api/authentication.py
@@ -1,6 +1,14 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 
+"""
+Steps:
+1. The user first logs in using username and password
+2. A token is generated and stored in DB if it's not already there for the user.
+3. Using this token, the user can make requests that they are authorized to make.
+4. Note: Even if the user has a token but they are not explicitly authorized to view, add, delete, change, etc, they still won't have access.
+"""
+
 
 class CustomTokenAuth(TokenAuthentication):
     # Replace "Token" with "Bearer", can use any keyword of our choice
