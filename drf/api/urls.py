96732d05baa738fb4c675025e9c4c73d988754a0
unknown <debmalyapaine@gmail.com>
2024-01-01 20:16:31 +0530

    add: comment

--- drf/api/urls.py
+++ drf/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
+# Go to /auth route and obtain the token directly for the user
 urlpatterns = [path("auth/", obtain_auth_token), path("", views.home, name="home")]
